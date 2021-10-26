8b7ec8de3c0f8b35fe46e014b4e0d57653f9c892
Arnold Lee <arnold.lee@pcc.edu>
2021-10-25 20:03:18 -0700

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,10 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Created by Arnold Lee on 10/25/2021.
+ */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
